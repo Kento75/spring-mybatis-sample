64f0509000004222cd4fe1c4b44f12e2cd788b0e
Kento Takano <kento2github@gmail.com>
2020-01-14 08:19:06 +0900

    Added postgresql-mybatis base app

---
+++ postgresql-mybatis/src/main/java/com/kento2github/postgresqlmybatis/PostgresqlMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.kento2github.postgresqlmybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresqlMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresqlMybatisApplication.class, args);
+	}
+
+}
---
+++ postgresql-mybatis/src/test/java/com/kento2github/postgresqlmybatis/PostgresqlMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kento2github.postgresqlmybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresqlMybatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
